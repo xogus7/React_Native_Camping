b6c0e679adbe77150c0467b48ad7f48027152ed8
xogus7 <xogus2676@gmail.com>
2024-08-02 22:03:49 +0900

    chore: install babel-plugin-module-resolver
    icons, images, components, pages 절대 경로 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@icons": "./src/assets/icons",
+          "@images": "./src/assets/images",
+          "@components": "./src/components",
+          "@pages": "./src/pages",
+
+        },
+      },
+    ],
+  ],
 };
