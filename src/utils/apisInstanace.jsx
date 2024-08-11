7ce85d922d3e1a2be66488bb8e3efa0b3b331e6c
xogus7 <xogus2676@gmail.com>
2024-08-11 23:33:56 +0900

    chore: ios 환경 테스트

--- src/utils/apisInstanace.jsx
+++ src/utils/apisInstanace.jsx
@@ -2,8 +2,16 @@ import { create } from 'apisauce';
 
 export const goCampInstance = create({
   baseURL: process.env.REACT_APP_GO_CAMPING_URL,
+  headers: {
+    'Content-type': 'Application/json',
+    Accept: 'Application/json',
+  },
 });
 
 export const swaggerInstance = create({
   baseURL: process.env.REACT_APP_SWAGGER_URL,
+  headers: {
+    'Content-type': 'Application/json',
+    Accept: 'Application/json',
+  },
 });
